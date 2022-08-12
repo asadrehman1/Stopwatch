6512f7e56e8114332ce370b4e996ad9a797f1ee9
Asad Rehman <106298043+asadrehman1@users.noreply.github.com>
2022-08-12 06:31:04 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+const second = document.getElementById("second")
+const milliSecond = document.getElementById("millisecond")
+let mCount=0;
+let sCount=0;
+
+start.addEventListener("click",function(){
+    myInterval = setInterval(() => {
+     mCount++;
+     if(mCount<99){
+        if(mCount<10){
+            milliSecond.innerText=`0${mCount}`;
+        }
+       else{
+        milliSecond.innerText=mCount;
+       }
+     }
+     else{
+        mCount=0;
+        sCount++;
+        if(sCount<10){
+            second.innerText=`0${sCount}`;
+        }
+       else{
+        second.innerText=sCount;
+       }
+     }
+   },10);
+})
+
+stop.addEventListener("click",function(){
+    clearInterval(myInterval);
+})
+
+reset.addEventListener("click",function(){
+    clearInterval(myInterval);
+    sCount=0;
+    mCount=0;
+    milliSecond.innerText=`0${0}`;
+    second.innerText=`0${0}`
+})
+
